def7f6d237adde51b4139a89c8724f460d84f583
MohammedElMO <elamranimohammed104@gmail.com>
2023-09-19 00:39:28 +0100

    sync

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -9,7 +9,10 @@ export default function useRoutes(app: Express) {
   app.use(json())
   app.use(
     cors({
-      origin: "https://gallery-ar8hf5x64-mohammedelmo.vercel.app",
+      origin: [
+        "https://gallery-ar8hf5x64-mohammedelmo.vercel.app",
+        "https://frontend-henna-alpha.vercel.app",
+      ],
       optionsSuccessStatus: 200,
     }),
   )
